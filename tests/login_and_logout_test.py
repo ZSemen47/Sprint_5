c062d0f1b8311fe2333628ff02bd6c445061b33c
Zhao Sen <zazulinsemen@gmail.com>
2026-03-28 00:42:36 +0300

    remove sleeps; add expected and actual result

--- tests/login_and_logout_test.py
+++ tests/login_and_logout_test.py
@@ -7,46 +7,43 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from faker import Faker
 import pytest
-from helpers import RegistrationOfAccount
+from helpers import (RegistrationOfAccount, EmailGeneration)
 from locators import (MainPageLocators, LoginAndRegistrationLocators, PlaceAnAddLocators)
 
 
 class TestLogin():
 
-    def test_login_user(self, driver, fake_email, fake_password):
+    def test_login_user(self, driver):
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located(MainPageLocators.ENTRY_AND_REGISTRATION_BUTTON))
-        driver.find_element(*MainPageLocators.ENTRY_AND_REGISTRATION_BUTTON).click()
-        driver.find_element(*LoginAndRegistrationLocators.THERE_IS_NO_ACCOUNT_BUTTON).click()
-        existing_email = fake_email
-        existing_password = fake_password
-        driver.find_element(*LoginAndRegistrationLocators.ENTER_EMAIL).send_keys(existing_email)
-        driver.find_element(*LoginAndRegistrationLocators.ENTER_PASSWORD).send_keys(existing_password)
-        driver.find_element(*LoginAndRegistrationLocators.REPEAT_PASSWORD).send_keys(existing_password)
-        driver.find_element(*LoginAndRegistrationLocators.CREATE_ACCOUNT_BUTTON).click()
+        existing_email = EmailGeneration.fake_email()
+        existing_password = EmailGeneration.fake_password()
+        RegistrationOfAccount.register_account(driver, existing_email, existing_password)
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located(MainPageLocators.AVATAR_OF_THE_USER))
         driver.find_element(*MainPageLocators.EXIT_BUTTON).click()
-        time.sleep(3)
+        WebDriverWait(driver, 5).until(expected_conditions.invisibility_of_element_located(MainPageLocators.EXIT_BUTTON))
         driver.find_element(*MainPageLocators.ENTRY_AND_REGISTRATION_BUTTON).click()
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located(LoginAndRegistrationLocators.ENTER_BUTTON))
         driver.find_element(*LoginAndRegistrationLocators.ENTER_EMAIL).send_keys(existing_email)
         driver.find_element(*LoginAndRegistrationLocators.ENTER_PASSWORD).send_keys(existing_password)
         driver.find_element(*LoginAndRegistrationLocators.ENTER_BUTTON).click()
-        WebDriverWait(driver, 3).until(
-            expected_conditions.visibility_of_element_located(MainPageLocators.AVATAR_OF_THE_USER))
-        assert driver.find_element(*MainPageLocators.AVATAR_OF_THE_USER).is_displayed()
-        assert driver.find_element(*MainPageLocators.NAME_OF_THE_USER).is_displayed()
+        WebDriverWait(driver, 3).until(expected_conditions.invisibility_of_element_located(LoginAndRegistrationLocators.ENTER_BUTTON))
+        expected_result = True
+        actual_result_1 = driver.find_element(*MainPageLocators.AVATAR_OF_THE_USER).is_displayed()
+        actual_result_2 = driver.find_element(*MainPageLocators.NAME_OF_THE_USER).is_displayed()
+        assert expected_result == actual_result_1
+        assert expected_result == actual_result_2
 
 
 class TestLogout():
-    def test_logout_user(self, driver, fake_email, fake_password):
+    def test_logout_user(self, driver):
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located(MainPageLocators.ENTRY_AND_REGISTRATION_BUTTON)).click()
         driver.find_element(*LoginAndRegistrationLocators.THERE_IS_NO_ACCOUNT_BUTTON).click()
-        existing_email = fake_email
-        existing_password = fake_password
+        existing_email = EmailGeneration.fake_email()
+        existing_password = EmailGeneration.fake_password()
         driver.find_element(*LoginAndRegistrationLocators.ENTER_EMAIL).send_keys(existing_email)
         driver.find_element(*LoginAndRegistrationLocators.ENTER_PASSWORD).send_keys(existing_password)
         driver.find_element(*LoginAndRegistrationLocators.REPEAT_PASSWORD).send_keys(existing_password)
@@ -54,7 +51,7 @@ class TestLogout():
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located(MainPageLocators.AVATAR_OF_THE_USER))
         driver.find_element(*MainPageLocators.EXIT_BUTTON).click()
-        time.sleep(3)
+        WebDriverWait(driver, 5).until(expected_conditions.invisibility_of_element_located(MainPageLocators.EXIT_BUTTON))
         WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable(MainPageLocators.ENTRY_AND_REGISTRATION_BUTTON)).click()
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(LoginAndRegistrationLocators.POPUP_LOGIN))
         WebDriverWait(driver, 5).until(
@@ -64,4 +61,6 @@ class TestLogout():
         WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located(MainPageLocators.AVATAR_OF_THE_USER))
         driver.find_element(*MainPageLocators.EXIT_BUTTON).click()
-        assert driver.find_element(*MainPageLocators.ENTRY_AND_REGISTRATION_BUTTON).is_displayed()
+        expected_result = True
+        actual_result = driver.find_element(*MainPageLocators.ENTRY_AND_REGISTRATION_BUTTON).is_displayed()
+        assert expected_result == actual_result
