cad485a01d180d84d093cdb895cd82140e7f49eb
Zhao Sen <zazulinsemen@gmail.com>
2026-03-28 00:40:53 +0300

    remove wrong fixtures

--- conftest.py
+++ conftest.py
@@ -1,32 +1,13 @@
 from selenium import webdriver
-from faker import Faker
 import pytest
-from locators import (MainPageLocators)
+from data import Data
 
 
 @pytest.fixture(scope='function')
 def driver():
     driver = webdriver.Chrome()
-    driver.get(MainPageLocators.MAIN_PAGE_URL)
+    driver.get(Data.main_page_url)
     try:
         yield driver
     finally:
         driver.quit()
-
-
-@pytest.fixture()
-def fake_email():
-    fake = Faker()
-    return fake.email()
-
-
-@pytest.fixture()
-def incorrect_fake_email():
-    fake = Faker()
-    return fake.domain_name()
-
-
-@pytest.fixture()
-def fake_password():
-    fake = Faker()
-    return fake.password()
